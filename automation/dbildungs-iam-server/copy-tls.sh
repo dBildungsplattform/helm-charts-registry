70333bddfe3ae4562f0b27d7076912695140792f
dbildungs-iam-server-gha <dbildungs-iam-server@dbildungsplattform.de>
2024-05-06 06:11:41 +0000

    dbildungs-iam-server

--- automation/dbildungs-iam-server/copy-tls.sh
+++ automation/dbildungs-iam-server/copy-tls.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
-
 STAGING_DIR="/staging"
-MOUNTED_FILES_DIR="tls"
-CONFIG_DIR="/data/tls"
-# Create the directories
-mkdir -p "$STAGING_DIR" || exit 1
-mkdir -p "$CONFIG_DIR"  || exit 1
+MOUNTED_FILES_DIR="/tls"
+CONFIG_DIR=/data/tls
 
-# Check if the TLS files are already generated
-if [ ! -f "$MOUNTED_FILES_DIR/ca.crt" ] || [ ! -f "$MOUNTED_FILES_DIR/redis.crt" ] || [ ! -f "$MOUNTED_FILES_DIR/redis.key" ]; then
-  echo "Generating TLS certificates using gencert.sh..."
-  cp -v "$MOUNTED_FILES_DIR/gencert.sh" "$STAGING_DIR"
-  pushd "$STAGING_DIR" || exit 1
-  chmod +x gencert.sh
-  ./gencert.sh
-  popd || exit 1
-fi
+# Setup working environment
 
-# Copy the TLS files to the staging directory
-echo "Copying TLS files to staging directory..."
-cp -v "$MOUNTED_FILES_DIR/ca.crt" "$MOUNTED_FILES_DIR/redis.crt" "$MOUNTED_FILES_DIR/redis.key" "$STAGING_DIR"
+mkdir -p $STAGING_DIR
+mkdir -p $CONFIG_DIR
+# Copy generator script
+cp -v $MOUNTED_FILES_DIR/gencert.sh /staging
 
-# Change to the staging directory
-echo "Changing to staging directory..."
-pushd "$STAGING_DIR" || exit 1
-# Copy the generated certificates to the config directory
-echo "Copying certificates to config directory..."
-cp -v ca.crt redis.crt redis.key "$CONFIG_DIR"
-# Change back to the previous directory
-echo "Changing back to previous directory..."
+# Begin Bracket
+pushd $STAGING_DIR || exit 1
+
+chmod ugo+x gencert.sh
+./gencert.sh
+
+# We have generated our certificates, now we put them in their right place
+cp -v tls/redis.crt tls/redis.key tls/ca.crt $CONFIG_DIR
+# Pre-Made config, correct certs and disabling of non-tls included
+cp -v $MOUNTED_FILES_DIR/redis.conf $CONFIG_DIR
+
+# End Bracket
 popd || exit 1
 
-# Remove the staging directory
-echo "Removing staging directory..."
-rm -rf "$STAGING_DIR"
+# $CONFIG_DIR remains behind, that is the point of this script, Staging is cleaned away
+rm -rf $STAGING_DIR
