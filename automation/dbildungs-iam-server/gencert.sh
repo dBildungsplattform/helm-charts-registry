8dd4ca072955127fd4e5b416817bb463ca6f2711
dbildungs-iam-server-gha <dbildungs-iam-server@dbildungsplattform.de>
2024-05-07 09:09:05 +0000

    dbildungs-iam-server

--- automation/dbildungs-iam-server/gencert.sh
+++ automation/dbildungs-iam-server/gencert.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# COPIED/MODIFIED from the redis server gen-certs util
+
 # Generate some test certificates which are used by the regression test suite:
 #
 #   tls/ca.{crt,key}          Self signed CA certificate.
@@ -8,16 +10,13 @@
 #   tls/server.{crt,key}      A certificate restricted for SSL server usage.
 #   tls/redis.dh              DH Params file.
 
-CERT_DIR="tls"
-mkdir -p $CERT_DIR || exit 1
-
 generate_cert() {
     local name=$1
     local cn="$2"
     local opts="$3"
 
-    local keyfile=$CERT_DIR/${name}.key
-    local certfile=$CERT_DIR/${name}.crt
+    local keyfile=tls/${name}.key
+    local certfile=tls/${name}.crt
 
     [ -f $keyfile ] || openssl genrsa -out $keyfile 2048
     openssl req \
@@ -26,35 +25,35 @@ generate_cert() {
         -key $keyfile | \
         openssl x509 \
             -req -sha256 \
-            -CA $CERT_DIR/ca.crt \
-            -CAkey $CERT_DIR/ca.key \
-            -CAserial $CERT_DIR/ca.txt \
+            -CA tls/ca.crt \
+            -CAkey tls/ca.key \
+            -CAserial tls/ca.txt \
             -CAcreateserial \
             -days 365 \
             $opts \
             -out $certfile
 }
 
-[ -f $CERT_DIR/ca.key ] || openssl genrsa -out $CERT_DIR/ca.key 4096
+mkdir -p tls
+[ -f tls/ca.key ] || openssl genrsa -out tls/ca.key 4096
 openssl req \
     -x509 -new -nodes -sha256 \
-    -key $CERT_DIR/ca.key \
+    -key tls/ca.key \
     -days 3650 \
     -subj '/O=Redis Test/CN=Certificate Authority' \
-    -out $CERT_DIR/ca.crt
+    -out tls/ca.crt
 
-cat > $CERT_DIR/openssl.cnf <<_END_
+cat > tls/openssl.cnf <<_END_
 [ server_cert ]
 keyUsage = digitalSignature, keyEncipherment
 nsCertType = server
-
 [ client_cert ]
 keyUsage = digitalSignature, keyEncipherment
 nsCertType = client
 _END_
 
-generate_cert server "Server-only" "-extfile $CERT_DIR/openssl.cnf -extensions server_cert"
-generate_cert client "Client-only" "-extfile $CERT_DIR/openssl.cnf -extensions client_cert"
+generate_cert server "Server-only" "-extfile tls/openssl.cnf -extensions server_cert"
+generate_cert client "Client-only" "-extfile tls/openssl.cnf -extensions client_cert"
 generate_cert redis "Generic-cert"
 
-[ -f $CERT_DIR/redis.dh ] || openssl dhparam -out $CERT_DIR/redis.dh 2048
+[ -f tls/redis.dh ] || openssl dhparam -out tls/redis.dh 2048
