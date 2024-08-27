ad1507108513b6fe355e5e2abe2417ef469efdd8
Jannik Badenhop <jannik.badenhop@capgemini.com>
2024-08-27 14:51:09 +0000

    iterate all charts and check them for dependencies

--- test.sh
+++ test.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-repos=()
-
-for file in $(find automation -name "Chart.yaml"); do
-    # Perform your desired operation on $file
-    echo "Processing $file"
-    repos+=$(yq eval '.dependencies[].repository' "$file" | grep '^https')
-done
+set -x
+repos=$(find automation -name "Chart.yaml" -exec yq '.dependencies[].repository' {} \; | \
+        sort | \
+        uniq | \
+        grep '^https')
 
 # Loop through repositories and add them
-for repo in "${repos[@]}"; do
+for repo in $repos; do
     echo "Adding repo \"$(basename "$repo")\" with source \"$repo\""
     helm repo add "$(basename "$repo")" "$repo" || echo "Failed to add \"$repo\""
 done
