8583b60043bc9538c1bc05da1c59f819d889a3e6
dbp-moodle-gha <dbp-moodle@dbildungsplattform.de>
2024-09-26 11:14:21 +0000

    dbp-moodle

--- automation/dbp-moodle/scripts/config.php
+++ automation/dbp-moodle/scripts/config.php
@@ -9,11 +9,7 @@ $CFG->dblibrary = 'native';
 $CFG->dbhost    = '{{ .Values.moodle.externalDatabase.host }}';
 $CFG->dbname    = '{{ .Values.moodle.externalDatabase.database }}';
 $CFG->dbuser    = '{{ .Values.moodle.externalDatabase.user }}';
-{{ if .Values.moodle.externalDatabase.existingSecret -}}
-$CFG->dbpass    = '{{ index (lookup "v1" "Secret" .Release.Namespace .Values.moodle.externalDatabase.existingSecret).data "mariadb-password" | b64dec }}';
-{{- else -}}
 $CFG->dbpass    = '{{ .Values.moodle.externalDatabase.password }}';
-{{- end }}
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
