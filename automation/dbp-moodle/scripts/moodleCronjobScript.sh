ac430fc52f6e23fb2cd85e9e42066b36db75c782
dbp-moodle-gha <dbp-moodle@dbildungsplattform.de>
2024-10-02 11:07:09 +0000

    dbp-moodle

--- automation/dbp-moodle/scripts/moodleCronjobScript.sh
+++ automation/dbp-moodle/scripts/moodleCronjobScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-moodle_pods=$(kubectl -n {{ .Release.Namespace }} get pods -l app.kubernetes.io/instance={{ .Release.Name }} -o jsonpath='{.items[*].metadata.name}')
+moodle_pods=$(kubectl -n {{ .Release.Namespace }} get pods -l app.kubernetes.io/name=moodle -o jsonpath='{.items[*].metadata.name}')
 
 for pod in "${moodle_pods[@]}"; do
   echo "Waiting for pod [${pod}] to be ready..."
-  kubectl -n {{ .Release.Namespace }} wait --for=condition=Ready pod/${pod} --timeout="{{ .Values.dbpMoodle.moodlecronjob.wait_timeout }}"
+  kubectl -n {{ .Release.Namespace }} wait --for=condition=Ready pod/${pod} --timeout={{ .Values.dbpMoodle.moodlecronjob.wait_timeout }}
   echo "Executing command in pod: ${pod}"
   kubectl exec -n {{ .Release.Namespace }} "${pod}" -- /opt/bitnami/php/bin/php ./bitnami/moodle/admin/cli/cron.php
   echo "Command '/opt/bitnami/php/bin/php ./bitnami/moodle/admin/cli/cron.php' has been run on pod [${pod}]!"
