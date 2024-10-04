97b3d6f61585903ce1236736065487a18c897e38
dbp-moodle-gha <dbp-moodle@dbildungsplattform.de>
2024-10-04 12:38:56 +0000

    dbp-moodle

--- automation/dbp-moodle/scripts/moodleCronjobScript.sh
+++ automation/dbp-moodle/scripts/moodleCronjobScript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
-# get kubectl command
-curl -LO https://dl.k8s.io/release/v{{ .Values.global.kubectl_version }}/bin/linux/amd64/kubectl
-chmod +x kubectl
-mv ./kubectl /usr/local/bin/kubectl
 
-kubectl get pods -n {{ .Release.Namespace }} | egrep "^moodle-[a-z0-9]{8,10}-[a-z0-9]{5,5}"
-kubectl exec $(kubectl get pods -n {{ .Release.Namespace }} --field-selector=status.phase=Running | egrep "^moodle-[a-z0-9]{5,10}-[a-z0-9]{3,5}[^a-z0-9]") -- /opt/bitnami/php/bin/php ./bitnami/moodle/admin/cli/cron.php
-echo "Command '/opt/bitnami/php/bin/php ./bitnami/moodle/admin/cli/cron.php' has been run!"
+moodle_pod=$(kubectl -n {{ .Release.Namespace }} get pods -l app.kubernetes.io/name=moodle -o jsonpath='{.items[0].metadata.name}')
+
+echo "Waiting for pod [${moodle_pod}] to be ready..."
+kubectl -n "{{ .Release.Namespace }}" wait --for=condition=Ready pod/"${moodle_pod}" --timeout={{ .Values.dbpMoodle.moodlecronjob.wait_timeout }}
+echo "Executing command in pod: ${moodle_pod}"
+kubectl exec -n {{ .Release.Namespace }} "${moodle_pod}" -- /opt/bitnami/php/bin/php ./bitnami/moodle/admin/cli/cron.php
+echo "Command '/opt/bitnami/php/bin/php ./bitnami/moodle/admin/cli/cron.php' has been run on pod [${moodle_pod}]!"
