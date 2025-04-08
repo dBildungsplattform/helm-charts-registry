d9a08b0d81e26da4bb269f9967bd69995a9ee22e
dbp-moodle-gha <dbp-moodle@dbildungsplattform.de>
2025-04-08 08:36:46 +0000

    dbp-moodle

--- automation/dbp-moodle/scripts/restore.sh
+++ automation/dbp-moodle/scripts/restore.sh
@@ -38,7 +38,7 @@ if [ -z "$replicas" ] || [ "$replicas" -eq 0 ]; then
     replicas=1
 fi
 echo "=== Scale moodle deployment to 0 replicas for restore operation ==="
-kubectl patch "deployment/${deployment_name}" -n "{{ .Release.Namespace }}" -p '{"spec":{"replicas": 0}}'
+kubectl scale "deployment/${deployment_name}" --replicas=0 -n {{ .Release.Namespace }}
 echo "=== After restore operation is completed will scale back to: $replicas replicas ==="
 
 # Restore
@@ -69,8 +69,8 @@ mv /bitnami/mountData/moodle/.[!.]* /bitnami/moodle/
 mv /bitnami/mountData/moodledata/* /bitnami/moodledata/
 mv /bitnami/mountData/moodledata/.[!.]* /bitnami/moodledata/
 # Set moodle user 1001
-# chown -R 1001 /bitnami/moodle
-# chown -R 1001 /bitnami/moodledata
+chown -R 1001 /bitnami/moodle
+chown -R 1001 /bitnami/moodledata
 
 cd /bitnami/
 echo "=== Clear DB ==="
@@ -100,8 +100,8 @@ PGPASSWORD="$DATABASE_PASSWORD" psql -h "$DATABASE_HOST" -p "$DATABASE_PORT" -U 
 {{- end }}
 echo "=== Finished DB restore ==="
 
-echo "=== Scaling deployment replicas to 1 ==="
-kubectl patch "deployment/${deployment_name}" -n "{{ .Release.Namespace }}" -p '{"spec":{"replicas": 1}}'
+echo "=== Scaling deployment replicas to $replicas ==="
+kubectl scale "deployment/${deployment_name}" --replicas=$replicas -n {{ .Release.Namespace }}
 sleep 2
 scaledTo=$(kubectl get "deployment/${deployment_name}" -n {{ .Release.Namespace }} -o=jsonpath='{.status.replicas}')
 echo "=== Deployment scaled to: $scaledTo ==="
