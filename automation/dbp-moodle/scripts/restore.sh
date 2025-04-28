97815c995ece1784a2758282968f0be2b4147898
dbp-moodle-gha <dbp-moodle@dbildungsplattform.de>
2025-04-28 11:59:36 +0000

    dbp-moodle

--- automation/dbp-moodle/scripts/restore.sh
+++ automation/dbp-moodle/scripts/restore.sh
@@ -20,7 +20,6 @@ health_file="/tmp/healthy"
 # Create liveness probe file
 touch "${health_file}"
 
-# Dieser Abschnitt funktioniert nicht
 {{ if and .Values.dbpMoodle.backup.s3_certificate_secret.enabled }}
 printf "Appendending custom certificate (%s/%s) to /etc/ssl/certs/ca-certificates.crt\n" "{{ .Values.dbpMoodle.backup.s3_certificate_secret.mountpath }}" "{{ .Values.dbpMoodle.backup.s3_certificate_secret.key }}"
 cat "{{ .Values.dbpMoodle.backup.s3_certificate_secret.mountpath }}/{{ .Values.dbpMoodle.backup.s3_certificate_secret.key }}" >> /etc/ssl/certs/ca-certificates.crt
