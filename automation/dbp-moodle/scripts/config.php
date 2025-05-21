99d92660dfa6be2ed214dbe5ccb7c042d8ef2726
dbp-moodle-gha <dbp-moodle@dbildungsplattform.de>
2025-05-21 09:55:30 +0000

    dbp-moodle

--- automation/dbp-moodle/scripts/config.php
+++ automation/dbp-moodle/scripts/config.php
@@ -55,6 +55,13 @@ $CFG->disableupdateautodeploy = true;
 {{- . }}
 {{- end }}
 
+{{- if .Values.clamav.enabled }}
+$CFG->forced_plugin_settings['antivirus']['checkantivirus'] = ''; # Still needs the correct value
+$CFG->forced_plugin_settings['antivirus_clamav']['runningmethod'] = 'tcpsocket';
+$CFG->forced_plugin_settings['antivirus_clamav']['tcpsockethost'] = 'clamav';
+$CFG->forced_plugin_settings['antivirus_clamav']['tcpsocketport'] = 3310;
+{{- end }}
+
 require_once(__DIR__ . '/lib/setup.php');
 
 {{- if .Values.dbpMoodle.phpConfig.extendedLogging }}
