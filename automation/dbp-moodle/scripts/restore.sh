2a2d5f634f7e7c28e7ef198697a0d227fa02552b
dbp-moodle-gha <dbp-moodle@dbildungsplattform.de>
2026-02-25 15:16:57 +0000

    dbp-moodle

--- automation/dbp-moodle/scripts/restore.sh
+++ automation/dbp-moodle/scripts/restore.sh
@@ -16,7 +16,6 @@ function clean_up() {
 trap "clean_up" EXIT
 
 health_file="/tmp/healthy"
-RESTORE_DATE="{{ .Values.dbpMoodle.restore.restoreDate }}"
 
 # Create liveness probe file
 touch "${health_file}"
@@ -58,10 +57,7 @@ cd /tmp/
 echo "=== Download backup ==="
 ln -s /etc/duply /home/nonrootuser/.duply
 export DUPLY_HOME="/etc/duply"
-
-# Duply restore logic
-/usr/bin/duply default restore Full "$RESTORE_DATE"
-
+/usr/bin/duply default restore Full
 echo "=== Clear PVC ==="
 rm -rf /dbp-moodle/moodle/*
 rm -rf /dbp-moodle/moodle/.[!.]*
